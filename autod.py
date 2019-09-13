02796292adca95d69a872867f15e58a0ae1bf140
MatarBachar <matanbach44@gmail.com>
2019-09-13 23:50:39 +0300

    Test for argparse

--- autod.py
+++ autod.py
@@ -10,9 +10,9 @@ def dir_exists(value):
 def get_args():
     parser = argparse.ArgumentParser(description='Short sample app')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file', metavar='FILENAME', type=argparse.FileType('r'),
+    group.add_argument('-f', '--file', metavar='FILENAME', type=argparse.FileType('r', encoding='utf8'),
                              help='Download multiple songs with a given list from file. Song names seperated by newlines')
-    group.add_argument('-s', '--single', metavar='URL', type=str
+    group.add_argument('-s', '--single', metavar='URL', type=str,
                              help='Download speceific song by URL')
     group.add_argument('-p', '--playlist', metavar='URL', type=str,
                              help='Download a whole playlist from youtube')
@@ -23,6 +23,11 @@ def get_args():
 
 
 def main():
+    args = get_args()
+    print(type(args.file), dir(args.file))
+    if args.file:
+        for song in args.file.readlines():
+            print(song, end='')
     youtube_searcher = CoolSearcher()
     for result in youtube_searcher.search("amon amarth twilight of the thunder god hq"):
         print(result)
