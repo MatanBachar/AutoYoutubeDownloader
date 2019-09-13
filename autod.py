6147e15ac532ca239a5400d263e1708d33275a57
MatarBachar <matanbach44@gmail.com>
2019-09-13 23:40:51 +0300

    Added parser

--- autod.py
+++ autod.py
@@ -1,4 +1,25 @@
+import argparse
+import os.path
 from searcher.cool_searcher import CoolSearcher
+
+def dir_exists(value):
+    if not os.path.exists(value):
+        raise argparse.ArgumentTypeError("Destination directory does not exists")
+    return value
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Short sample app')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-f', '--file', metavar='FILENAME', type=argparse.FileType('r'),
+                             help='Download multiple songs with a given list from file. Song names seperated by newlines')
+    group.add_argument('-s', '--single', metavar='URL', type=str
+                             help='Download speceific song by URL')
+    group.add_argument('-p', '--playlist', metavar='URL', type=str,
+                             help='Download a whole playlist from youtube')
+    parser.add_argument('dest'           , metavar='DEST_DIR', type=dir_exists,
+                             help='Destination directory for your new songs')
+
+    return parser.parse_args()
 
 
 def main():
