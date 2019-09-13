7e03ee939f7ef5fc3301c6acb4849a92488075ce
MatarBachar <matanbach44@gmail.com>
2019-09-13 15:26:00 +0300

    funcsss

--- autod.py
+++ autod.py
@@ -2,7 +2,22 @@ import urllib.request
 import urllib.parse
 import re
 
-query_string = urllib.parse.urlencode({"search_query" : input()})
-html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
-search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
-print("http://www.youtube.com/watch?v=" + search_results[0])
+
+
+
+
+def search_youtube(query):
+    query_string = urllib.parse.urlencode({"search_query" : input()})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    for result in search_results:
+        yield result
+
+def main():
+    query_string = urllib.parse.urlencode({"search_query" : input()})
+    html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
+    search_results = re.findall(r'href=\"\/watch\?v=(.{11})', html_content.read().decode())
+    
+
+if __name__ == "__main__":
+    main()
