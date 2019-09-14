d813804eb50643080d984549d0dd3112ea5a5487
MatarBachar <matanbach44@gmail.com>
2019-09-14 11:17:00 +0300

    main intergrate

--- autod.py
+++ autod.py
@@ -11,36 +11,16 @@ def dir_exists(value):
 def get_args():
     parser = argparse.ArgumentParser(description='Short sample app')
     group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file', metavar='FILENAME',      type=argparse.FileType('r', encoding='utf8'),
+    group.add_argument('-f', '--file',          metavar='FILENAME',           type=argparse.FileType('r', encoding='utf8'),
                              help='Download multiple songs with a given list from file. Song names seperated by newlines')
-    group.add_argument('-s', '--single-byurl', metavar='URL',   type=str,
-                             help='Download speceific song by URL')
-    group.add_argument('-n', '--single-byname', metavar='NAME', type=str,
-                             help='Download speceific song by URL')
-    group.add_argument('-p', '--playlist', metavar='URL',       type=str,
+    group.add_argument('-s', '--single',        metavar='NAME',               type=str,
+                             help='Download speceific song by name')
+    group.add_argument('-p', '--playlist',      metavar='URL playlist_name',  type=str,   nargs=2,
                              help='Download a whole playlist from youtube')
-    parser.add_argument('dest'           , metavar='DEST_DIR',  type=dir_exists,
+    parser.add_argument('-o',                   action='store_true',    default=False,   dest="override",
+                             help='Override existing songs')
+    parser.add_argument('dest',                 metavar='DEST_DIR',     type=dir_exists,
                              help='Destination directory for your new songs')
-
-    return parser.parse_args()
-
-def dir_exists(value):
-    if not os.path.exists(value):
-        raise argparse.ArgumentTypeError("Destination directory does not exists")
-    return value
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Short sample app')
-    group = parser.add_mutually_exclusive_group(required=True)
-    group.add_argument('-f', '--file', metavar='FILENAME', type=argparse.FileType('r', encoding='utf8'),
-                             help='Download multiple songs with a given list from file. Song names seperated by newlines')
-    group.add_argument('-s', '--single', metavar='URL', type=str,
-                             help='Download speceific song by URL')
-    group.add_argument('-p', '--playlist', metavar='URL', type=str,
-                             help='Download a whole playlist from youtube')
-    parser.add_argument('dest'           , metavar='DEST_DIR', type=dir_exists,
-                             help='Destination directory for your new songs')
-
     return parser.parse_args()
 
 
@@ -52,17 +32,15 @@ def main():
 
     if args.file:
         for song in args.file.readlines():
-            print(song, end='')
-            result = next(youtube_searcher.search(song + "hq"))
-
-    # if args.single:
-    #     youtube_downloader.download()
+            print(song.strip())
+            result = next(youtube_searcher.search(song + " hq" if song.isalpha() else song))
+            print(result)
+            youtube_downloader.download(result, song.strip(), args.dest, args.override)
+    if args.single:
+        result = next(youtube_searcher.search(args.single.strip() + " hq" if args.single.isalpha() else args.single))
+        youtube_downloader.download(result, args.single.strip(), args.dest, args.override)
     if args.playlist:
-        pass
-    
-    for result in youtube_searcher.search("amon amarth twilight of the thunder god hq"):
-        print(result)
-    
+        youtube_downloader.download_playlist(args.playlist[0], args.playlist[1], args.dest, args.override)
 
 if __name__ == "__main__":
     main()
